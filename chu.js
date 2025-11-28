5e210f8377bd564541d0ed6a1a7956d76d7f71e0
DnkllerX <116860700+DnkllerX@users.noreply.github.com>
2025-11-28 10:21:38 +0700

    add hieu ung

---
+++ chu.js
@@ -0,0 +1,22 @@
+const text = "♡vo iu PhuongThao";
+const textEl = document.getElementById("text");
+let i = 0;
+
+function fadeInText() {
+  if(i < text.length){
+    const span = document.createElement("span");
+    span.textContent = text[i];
+    span.classList.add("fade-in");
+    textEl.appendChild(span);
+
+    // Trigger fade-in
+    setTimeout(() => {
+      span.classList.add("show");
+    }, 50);
+
+    i++;
+    setTimeout(fadeInText, 300); // tốc độ fade từng chữ
+  }
+}
+
+fadeInText();
